b38b20ca20d69daf840233d7bc950aa1f2ed48a2
ManuelFranco <81265002+ManuelFranco@users.noreply.github.com>
2024-09-06 09:30:07 +0200

    Update __init__.py

--- tg_shap/__init__.py
+++ tg_shap/__init__.py
@@ -1,3 +1,4 @@
 # my_shap_package/__init__.py
 from .tg_shap import tg_shap
-from .utils import all_subsets, all_subsets_without
+from .utils import all_subsets, all_subsets_without, generateRandomSubsets
+from .tg_shap import approx_tg_shap
