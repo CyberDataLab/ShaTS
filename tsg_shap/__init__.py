a0597abbde280631c30b03abe8f63881e8716b96
ManuelFranco <manuel.francop@um.es>
2025-01-17 12:40:34 +0100

    Fixed init

--- tsg_shap/__init__.py
+++ tsg_shap/__init__.py
@@ -1,3 +1,3 @@
 # my_shap_package/__init__.py
-from .utils import generate_subsets, StrategySubsets, StrategyGrouping, StrategyPrediction
+from .utils import generate_subsets, StrategySubsets, StrategyGrouping, StrategyPrediction, StrategyValue
 from .tsg_shap import TSG_SHAP
